39a3f8efa0e62e508019ccabef4fbabb037ef492
Gourab Kundu <91219889+voltme@users.noreply.github.com>
2024-08-19 14:41:12 +0000

    arithmetic operations

---
+++ javascript1/arithmeticop.js
@@ -0,0 +1,15 @@
+console.log(5+5); //ADDITION
+
+console.log(10-5);//SUBSTRACTION
+
+console.log(5*5);//MULTIPLICATION
+
+console.log(10/2);//DIVISION
+
+console.log(10%5);//MODULUS
+
+console.log(2**2);//POWER
+
+
+
+
